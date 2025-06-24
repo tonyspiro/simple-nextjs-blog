f708d434e5ae73e75da494a990b97b8554c7af7d
Tony Spiro <tspiro@tonyspiro.com>
2025-06-24 12:28:49 -0700

    Update header to full width and add authors/categories sections to home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ import { GlobalData } from '../lib/types';
 
 export default function Header({ name }: { name: GlobalData }): JSX.Element {
   return (
-    <header className="sticky top-0 z-10 mx-auto bg-white/75 backdrop-blur-lg dark:bg-zinc-950/75">
-      <div className="mx-auto max-w-3xl px-4 lg:px-0">
+    <header className="sticky top-0 z-10 w-full bg-white/75 backdrop-blur-lg dark:bg-zinc-950/75">
+      <div className="w-full px-4 lg:px-8">
         <div className="flex items-center justify-between py-4">
           <SiteLogo siteData={name} />
           <nav className="flex items-center space-x-6">
